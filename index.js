076fbb1edcafc0998fff683ce867c1fc06d72351
soniachahal <75319532+soniachahal@users.noreply.github.com>
2020-12-21 19:34:30 +0545

    Rain, cloud and GUI

--- index.js
+++ index.js
@@ -13,11 +13,31 @@ var ringParent;
 let score = 0;
 let lives = 3;
 
+const RAIN_PARTICLES_COUNT = 100;
+const CLOUD_COUNT = 10;
+const RAIN_AREA_X = 10;
+const RAIN_AREA_Y = 14;
+const RAIN_AREA_Z = 50;
+const GROUND_Y = - 3;
+
+// MY CODE
+let guiObject, gui;
+let rainParticles, clouds;
+
+
+
 //#region WORLD SETUP
 function createWorld() {
   scene = new THREE.Scene();
   world = new CANNON.World();
   world.gravity.set(0, -1.2, 0); // m/s²
+
+  guiObject = {};
+  gui = new dat.GUI();
+
+  createScene();
+
+
 
   birdBody = new CANNON.Body({
     mass: 1, // kg
@@ -43,11 +63,11 @@ function createWorld() {
     scene.background = rt;
   }); */
 
-  var light;
+  /*var light;
   light = new THREE.DirectionalLight();
   light.position.set(0, 0, 1);
   camera.add(light);
-  scene.add(camera);
+  scene.add(camera);*/
 
   //------------------- Visible objects ----------------------
 
@@ -148,9 +168,222 @@ function updateForFrame() {
 function doFrame() {
   frameNumber++;
   if (bird != null) updateForFrame();
+
+  updateScene();
+
   renderer.render(scene, camera);
   requestAnimationFrame(doFrame);
   world.step(timeStep);
+}
+
+function updateScene() {
+
+  groundTexture.offset.y += 0.02;
+
+  const objectMoveSpeed = 0.05;
+/*
+  // updates the smoke particles
+  for ( let i = 0; i < smokeParticles.children.length; i ++ ) {
+
+    const particle = smokeParticles.children[ i ];
+
+    particle.scale.setScalar( particle.scale.x * 0.95 );
+
+    particle.position.y += particle.scale.x * 0.2;
+
+  }
+*/
+  // updates the rain particles
+  for ( let i = 0; i < rainParticles.children.length; i ++ ) {
+
+    const particle = rainParticles.children[ i ];
+
+    particle.translateY( - objectMoveSpeed * particle.speed * 2 );
+    particle.position.z += 0.1;
+
+    // if the rain particle is outside the screen, then move the rain particle 
+    // to the top of the screen
+    if ( particle.position.z > particle.scale.y * 5 ) {
+
+      particle.position.z = - RAIN_AREA_Z;
+
+    }
+
+    if ( particle.position.y < GROUND_Y - particle.scale.y ) {
+
+      particle.position.y = RAIN_AREA_Y;
+
+    }
+
+  }
+
+  // update clounds
+
+  for ( let i = 0; i < clouds.children.length; i ++ ) {
+
+    const cloud = clouds.children[ i ];
+
+    let v = cloud.speed * objectMoveSpeed;
+    cloud.position.z += v;
+
+    // if the cloud moves outside the view, move it at the right of the screen
+
+    if ( cloud.position.z > 0 ) {
+
+      cloud.position.z = - RAIN_AREA_Z;
+
+    }
+
+  }
+
+}
+
+function createScene() {
+
+  const bgColor = new THREE.Color( 'lightblue' );
+  scene.background = bgColor;
+  scene.fog = new THREE.Fog( bgColor, 10, 50 );
+
+  guiObject[ 'dir light color' ] = '#fff';
+  guiObject[ 'dir light intensity' ] = 0.5;
+
+  guiObject[ 'ambient light color' ] = '#fff';
+  guiObject[ 'ambient light intensity' ] = 0.5;
+
+  // lighting
+
+  const dirLight = new THREE.DirectionalLight( guiObject[ 'dir light color' ], guiObject[ 'dir light intensity' ] );
+  dirLight.position.set( 50, 60, 50 );
+  dirLight.castShadow = true;
+  scene.add( dirLight );
+
+  const ambientLight = new THREE.AmbientLight( guiObject[ 'ambient light color' ], guiObject[ 'ambient light intensity' ] );
+  scene.add( ambientLight );
+
+  // add gui components for the lights
+
+  gui.addColor( guiObject, 'dir light color' ).onChange( function ( value ) {
+
+    dirLight.color.set( value );
+
+  } );
+
+  gui.add( guiObject, 'dir light intensity', 0, 1.0, 0.05 ).onChange( function ( value ) {
+
+    dirLight.intensity = value;
+
+  } );
+
+  gui.addColor( guiObject, 'ambient light color' ).onChange( function ( value ) {
+
+    ambientLight.color.set( value );
+
+  } );
+
+  gui.add( guiObject, 'ambient light intensity', 0, 1.0, 0.05 ).onChange( function ( value ) {
+
+    ambientLight.intensity = value;
+
+  } );
+
+  // initialize the ground texture
+  groundTexture = new THREE.CanvasTexture( GroundTexture() );
+
+  // set the repeat amount
+  groundTexture.repeat.setScalar( 60 );
+
+  // make the texture to repeat
+  groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+  groundTexture.minFilter = groundTexture.magFilter = THREE.NearestFilter;
+
+  // ground
+  const groundColor = '#0f0';
+
+  const groundMaterial = new THREE.MeshLambertMaterial( {
+    map: groundTexture,
+    transparent: true,
+    color: groundColor
+  } );
+
+  // add gui component for the color of the ground
+
+  guiObject[ 'grass color' ] = groundColor;
+
+  gui.addColor( guiObject, 'grass color' ).onChange( function ( value ) {
+
+    groundMaterial.color.set( value );
+
+  } );
+
+  // create the ground and add it to the scene
+  const ground = new THREE.Mesh( new THREE.PlaneBufferGeometry( 150, 150 ), groundMaterial );
+  ground.receiveShadow = true;
+  ground.rotation.x = - Math.PI / 2;
+  ground.position.y = GROUND_Y;
+  scene.add( ground );
+
+  // create a wrapper object for the rain particles
+  rainParticles = new THREE.Object3D();
+  scene.add( rainParticles );
+
+  // rain geometry and material
+  const boxGeometry = new THREE.BoxBufferGeometry();
+  const rainMaterial = new THREE.MeshPhongMaterial( { color: 'blue' } );
+
+  // add random rain particles
+  for ( let i = 0; i < RAIN_PARTICLES_COUNT; i ++ ) {
+
+    const mesh = new THREE.Mesh( boxGeometry, rainMaterial );
+    mesh.position.x = - RAIN_AREA_X + Math.random() * RAIN_AREA_X * 2;
+    mesh.position.y = Math.random() * RAIN_AREA_Y * 2;
+    mesh.rotation.x = - Math.PI / 4;
+    mesh.position.z = - Math.random() * RAIN_AREA_Z;
+    mesh.speed = Math.random() + 0.5;
+    mesh.scale.set( 0.2, 1.5, 0.2 );
+    rainParticles.add( mesh );
+
+  }
+
+  // wrapper object for the clouds
+  clouds = new THREE.Object3D();
+  clouds.position.y = RAIN_AREA_Y * 0.2;
+  scene.add( clouds );
+
+  const cloudMaterial = new THREE.MeshLambertMaterial( { color: 'white' } );
+
+  // add random clouds
+  for ( let i = 0; i < CLOUD_COUNT; i ++ ) {
+
+    const cloud = new THREE.Mesh( boxGeometry, cloudMaterial );
+    cloud.position.x = Math.random() * RAIN_AREA_X * 2 - RAIN_AREA_X;
+    cloud.position.z = Math.random() * RAIN_AREA_Z * - 1;
+    //cloud.position.y = Math.random();
+    cloud.scale.set( Math.random() * 1 + 0.5, Math.random() * 0.4 + 0.3, Math.random() * 1 + 1 );
+    cloud.speed = Math.random() * 0.2 + 0.2;
+    clouds.add( cloud );
+
+  }
+
+}
+
+function GroundTexture() {
+
+  const canvas = document.createElement( 'canvas' );
+  canvas.width = canvas.height = 64;
+
+  const context = canvas.getContext( '2d' );
+
+  context.fillStyle = '#fff';
+  context.fillRect( 0, 0, canvas.width, canvas.height );
+
+  context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+  context.fillRect( 10, 10, 30, 30 );
+
+  context.fillStyle = 'rgba(0, 0, 0, 0.3)';
+  context.fillRect( 45, 45, 15, 15 );
+
+  return canvas;
+
 }
 
 //#region PC CONTROLS
